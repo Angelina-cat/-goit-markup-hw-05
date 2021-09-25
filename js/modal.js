92cb40ca9b9b0773e9d4c3ea30338f2e6abe884a
Angelina-cat <83021345+Angelina-cat@users.noreply.github.com>
2021-09-25 10:21:11 +0300

    о
    о

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,6 @@
 
-
-(() => {
-  const refs = {
+ (() =>
+  {const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
